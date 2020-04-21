59f9f8d3767a3a4a640f16b104b54a932684c5de
Luke Ahrens-Townsend <lahrenstownsend@gmail.com>
2020-04-21 14:55:14 -0500

    Fix build version 1.0.2
    Compressed Unity assets

--- Assets/Scripts/SkyBoxController.cs
+++ Assets/Scripts/SkyBoxController.cs
@@ -3,15 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SkyBoxController : MonoBehaviour
-{
-
-    public Material[] skyboxes;
+public class SkyBoxController : MonoBehaviour {
 
     private float skyboxRotateSpeed = 0.6f;
 
     void Awake() {
-        RenderSettings.skybox = skyboxes[Random.Range(0, (skyboxes.Length - 1))];
         gameObject.transform.rotation = Random.rotation;
         skyboxRotateSpeed = 1f;
     }
